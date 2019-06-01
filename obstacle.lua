9ae062ee69db642714b432ec0fb9a48944f592c5
Fabien <contested.space@gmail.com>
2019-06-01 18:17:31 -0400

    Now with falling bawks

--- obstacle.lua
+++ obstacle.lua
@@ -1,7 +1,7 @@
 Obstacle = {}
 Obstacle.__index = Obstacle
 
-function Obstacle:new(position, speed, delay)
+function Obstacle:new(position, speed, delay, generator)
    local o
    o = {}
    setmetatable(o, Obstacle)
@@ -11,6 +11,8 @@ function Obstacle:new(position, speed, delay)
    o.width = size
    o.height = size
    o.speed = speed
+   o.generator = generator
+   o.imageFile = love.graphics.newImage("img/Bawks.png")
 
    return o
 end
@@ -18,9 +20,16 @@ end
 
 function Obstacle:update(dt)
    self.y = self.y + self.speed * dt
+   if self.y > windowHeight then
+      gen:destroy(self)
+   end
 end
 
 
 function Obstacle:draw()
-   love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+   -- love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+   love.graphics.draw(self.imageFile, self.x, self.y, 0, 1, 1, 0, 0, 0, 0)
+end
+
+function Obstacle:hit_player()
 end
--- obstacle_generator.lua
+++ obstacle_generator.lua
@@ -7,30 +7,52 @@ function ObstacleGenerator:new()
    local og
    og = {}
    setmetatable(og, ObstacleGenerator)
-   og.updateable = {}
-   og.drawable = {}
+
+   og.obstacles = {}
+   
    
    return og
 end
 
 function ObstacleGenerator:trigger_full_line()
-   for i = 0, gridWidth, 1
+   for i = 0, 10, 1
    do
       obs = Obstacle:new(i, 100)
-      table.insert(self.updateable, obs)
-      table.insert(self.drawable, obs)
+      -- table.insert(self.updateable, obs)
+      -- table.insert(self.drawable, obs)
+      self.obstacles[obs] = 1
    end
 end
 
 function ObstacleGenerator:update(dt)
-   for _, value in next, self.updateable do
-      value:update(dt)
+   for obs, _ in next, self.obstacles do
+      if obs ~= nil then
+	 obs:update(dt)
+      end
    end
 end
 
 
 function ObstacleGenerator:draw(dt)
-   for _, value in next, self.drawable do
-      value:draw(dt)
+   for obs, _ in next, self.obstacles do
+      if obs ~= nil then
+	 obs:draw(dt)
+      end
    end
+end
+
+function ObstacleGenerator:destroy(obs)
+   if obs ~= nil then
+      self.obstacles = remove_element(self.obstacles, obs)
+   end
+end
+
+function remove_element(t, k)
+   output = {}
+   for obs, val in next, t do
+      if k ~= obs then
+	 output[obs] = val
+      end
+   end
+   return output
 end
